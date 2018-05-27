b97aec51cb8c92c6661dd30860ecd57b60b080b9
sergevs <abricus@yandex.ru>
2018-05-28 00:14:56 +0300

    allow to spawn multiply instances to test distributed collectors

--- zenoss.tf
+++ zenoss.tf
@@ -2,13 +2,17 @@ variable "region_zone" {
   default = "us-west1-a"
 }
 
+variable "the_count" {
+  default = "1"
+}
+
 resource "google_compute_firewall" "default" {
   name = "zenoss-firewall"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports = ["22","80","443"]
+    ports = ["22","80","443","4979"]
   }
 
   source_ranges = ["0.0.0.0/0"]
@@ -16,17 +20,20 @@ resource "google_compute_firewall" "default" {
 }
 
 resource "google_compute_disk" "zenoss-disk" {
-  name = "zenoss-disk"
+  count = "${var.the_count}"
+  name = "zenoss-disk-${count.index}"
   size = 40
   zone = "${var.region_zone}"
 }
 
 resource "google_compute_instance" "default" {
-  name = "zenoss"
+  count = "${var.the_count}"
+  name = "zenoss${count.index}"
   machine_type = "n1-highmem-4"
   zone = "${var.region_zone}"
   tags = ["zenoss"]
   depends_on = ["google_compute_disk.zenoss-disk"]
+
   boot_disk {
     initialize_params {
       image = "centos-7-v20180507"
@@ -34,7 +41,7 @@ resource "google_compute_instance" "default" {
   }
 
   attached_disk {
-    source = "zenoss-disk"
+    source = "zenoss-disk-${count.index}"
     device_name = "data"
   }
 
@@ -46,9 +53,9 @@ resource "google_compute_instance" "default" {
 }
 
 output "instance_id" {
- value = "${google_compute_instance.default.self_link}"
+ value = "${google_compute_instance.default.*.self_link}"
 }
 
 output "nat_ip" {
- value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+ value = "${google_compute_instance.default.*.network_interface.0.access_config.0.nat_ip}"
 }
